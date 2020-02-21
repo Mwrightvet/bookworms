dcb0a4f03309b0ea662f0bf9f8a289a0c30d59a2
Melissa <meliswright925@gmail.com>
2020-02-20 19:29:43 -0800

    updating with PK

--- schema_bookworm.sql
+++ schema_bookworm.sql
@@ -4,14 +4,14 @@ DROP TABLE IF EXISTS books_ratings CASCADE;
 
 
 CREATE TABLE "books" (
-    "isbn" VARCHAR(255),
+    "isbn" VARCHAR(255) PRIMARY KEY,
 	"book_title" TEXT,
 	"book_author" TEXT 
   );
 
 CREATE TABLE "ratings" (
     "user_id" VARCHAR(255)   NOT NULL,
-    "isbn" VARCHAR(255)   NOT NULL,
+    "isbn" VARCHAR(255)  NOT NULL PRIMARY KEY,
 	"book_rating" INT 
 );
 
@@ -27,3 +27,11 @@ SELECT * FROM books
 SELECT * FROM books_ratings
 SELECT * FROM ratings 
 	WHERE "isbn" = '034545104X';
+	
+SELECT COUNT (DISTINCT "isbn") AS "ratings_total" FROM "books_ratings"
+
+SELECT COUNT (DISTINCT "isbn") FROM "books"
+
+SELECT COUNT (DISTINCT "book_rating") FROM "books_ratings"
+
+
